7c02db6c35d0d618bdf570a2177337373fc3d220
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 10:45:56 +0900

    jsx

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+); */
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <h1>Hello, world!</h1>;
+root.render(element);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
