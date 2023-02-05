02a117b542d9eb91ee82c43b7c4cd3131097a93e
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 12:25:09 +0900

    Toggle component

--- src/index.js
+++ src/index.js
@@ -2,24 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Clock from './Clock';
+import Toggle from './Toggle';
 import reportWebVitals from './reportWebVitals';
 
-function Welcome(props){
-  return <h1>Hello, {props.name}</h1>
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-function App(){
+function App() {
   return (
     <div>
       <Welcome name="Sara" />
       <Welcome name="Cahal" />
       <Welcome name="Edite" />
     </div>
-  )
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Clock />);
+root.render(<Toggle />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
