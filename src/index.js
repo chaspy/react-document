c6dddb16558ae2ba82b51116d1dc5c4d2bbb3bdd
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 11:39:06 +0900

    コンポーネントを組み合わせる

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 function Welcome(props){
   return <h1>Hello, {props.name}</h1>
 }
 
+function App(){
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = <Welcome name="Sara" />
-root.render(element);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
