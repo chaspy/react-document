b363d22723d1bc659464e735f173d396230346d8
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 13:31:12 +0900

    NameForm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NumberList from './NumberList';
+import NameForm from './NameForm';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const numbers = [1, 2, 3, 4, 5];
-root.render(<NumberList numbers={numbers} />);
+root.render(<NameForm numbers={numbers} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
