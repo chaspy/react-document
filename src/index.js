ba5ac6282464f25a9c83c74dc21bf0e6aa68a6d0
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 11:03:24 +0900

    getGreeting

--- src/index.js
+++ src/index.js
@@ -15,13 +15,20 @@ function formatName(user){
   return user.firstName + ' '  + user.lastName
 }
 
+function getGreeting(user){
+  if (user){
+    return <h1>Hello, {formatName(user)}!</h1>
+  }
+  return <h1>Hello,Stranger.</h1>
+}
+
 const user = {
   firstName: 'Harper',
   lastName: 'Perez'
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = (<h1>Hello, {formatName(user)}!</h1>);
+const element = (getGreeting(user));
 root.render(element);
 
 
