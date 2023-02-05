88fec3f5bd05072f432b5e2492305949dd7edb24
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 13:06:48 +0900

    Remove unused

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Clock from './Clock';
-import Toggle from './Toggle';
-import Greeting from './Greeting';
 import LoginControl from './LoginControl';
 import reportWebVitals from './reportWebVitals';
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
-  );
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<LoginControl isLoggedIn={true} />);
