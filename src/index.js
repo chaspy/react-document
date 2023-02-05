2bc51aa58bb8ee75318d1948b46ef5c809caa29c
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 11:35:05 +0900

    コンポーネントのレンダー

--- src/index.js
+++ src/index.js
@@ -4,48 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-/* const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-); */
-
-function formatName(user){
-  return user.firstName + ' '  + user.lastName
-}
-
-function getGreeting(user){
-  if (user){
-    return <h1>Hello, {formatName(user)}!</h1>
-  }
-  return <h1>Hello,Stranger.</h1>
-}
-
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = (
-<div>
-  {getGreeting(user)}
-  <h2>Good to see you here.</h2>
-</div>);
+const element = <Welcome name="Sara" />
 root.render(element);
-
-function tick(){
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  )
-  root.render(element)
-}
-
-setInterval(tick, 1000)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
