3512244e8b2ca7a1a597cf8738aff983b9471808
kondo takeshi <chaspy@users.noreply.github.com>
2023-02-05 13:13:06 +0900

    基本的なリストコンポーネント

---
+++ src/NumberList.js
@@ -0,0 +1,7 @@
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) => <li>{number}</li>);
+  return <ul>{listItems}</ul>;
+}
+
+export default NumberList;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginControl from './LoginControl';
+import NumberList from './NumberList';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<LoginControl isLoggedIn={true} />);
+const numbers = [1, 2, 3, 4, 5];
+root.render(<NumberList numbers={numbers} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
